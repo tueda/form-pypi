02ec2352e523a2db1708c1bcec6f9208f8af62e7
Takahiro Ueda <t.ueda.od@juntendo.ac.jp>
2026-06-30 23:08:37 +0900

    chore: add pre-commit formatting and linting checks

--- form_bin/__main__.py
+++ form_bin/__main__.py
@@ -1,10 +1,13 @@
-from . import form_command
 import os
 import sys
 
+from . import form_command
+
+
 def main() -> None:
-    os.execv(form_command[0], form_command + sys.argv[1:])
-    exit(11)
+    os.execv(form_command[0], form_command + sys.argv[1:])  # noqa: S606
+    sys.exit(11)
+
 
 if __name__ == "__main__":
     main()
