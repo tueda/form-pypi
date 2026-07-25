4c34e95dfbb3f555d1d7ab55ca54684191c6cf90
Takahiro Ueda <t.ueda.od@juntendo.ac.jp>
2026-07-25 17:48:46 +0900

    chore: add pre-commit formatting and linting checks

--- form_bin/__init__.py
+++ form_bin/__init__.py
@@ -1,10 +1,11 @@
-__all__ = ("form_command", "form")
+__all__ = ("form", "form_command")
 
 import os
+import tempfile
+
 
 def _form_cmd() -> list[str]:
-    """
-    Determine the path and the needed set of arguments to call
+    """Determine the path and the needed set of arguments to call
     the form binary. Make sure the include paths are set up so
     that form packages installed into the Python environment are
     found.
@@ -14,15 +15,18 @@ def _form_cmd() -> list[str]:
     if not packages.endswith(os.path.sep):
         packages += os.path.sep
     form_path = os.path.join(this_dir, "tform")
-    tmp_dir = os.environ.get("TMPDIR", "/tmp")
+    tmp_dir = tempfile.gettempdir()
     form_tmp = os.environ.get("FORMTMP", tmp_dir)
     form_tmpsort = os.environ.get("FORMTMPSORT", tmp_dir)
     return [form_path, "-I", packages, "-t", form_tmp, "-ts", form_tmpsort]
 
-def form(*args: tuple[str]) -> None:
+
+def form(*args: str) -> None:
     """Run the form binary with the given arguments."""
     import subprocess
-    subprocess.check_call(form_command + list(args))
+
+    subprocess.check_call(form_command + list(args))  # noqa: S603
+
 
 # This is the path and the needed set of arguments to call the
 # form binary.
